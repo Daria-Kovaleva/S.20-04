cb599d670468e3f22213da052a1c17890a05f8c5
Darya Kovaleva <dasha_kovaleva@mail.ru>
2023-04-28 17:00:31 +0300

    Task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,3 +5,25 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number <= 99999)
+{
+    bool palindrome = Palindrome(number);
+    Console.Write(palindrome ? $"{number} -> да" : $"{number} -> нет");
+
+    bool Palindrome(int num)
+    {
+        int firstDigit = num / 10000;
+        int fifthDigit = num % 10;
+        int secondDigit = (num / 1000) % 10;
+        int fourthDigit = (num / 10) % 10;
+        if (firstDigit == fifthDigit && secondDigit == fourthDigit)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+else Console.WriteLine("Неккоректный ввод! Введите пятизначное число");
