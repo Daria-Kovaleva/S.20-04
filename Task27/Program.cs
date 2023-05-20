f5dd2c20e7e0e870651fba8d09c64c920240e813
Darya Kovaleva <dasha_kovaleva@mail.ru>
2023-05-20 21:16:55 +0300

    Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = number * (-1);
+
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма цифр в числе {number} -> {result}");
+
+int SumNumbers(int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        res = res + num % 10;
+        num = num / 10;
+    }
+    return res;
+}
