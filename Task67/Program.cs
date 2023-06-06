25db6f4c67209bf019a62e780f2ee7eba85eb014
Darya Kovaleva <dasha_kovaleva@mail.ru>
2023-06-06 16:08:32 +0300

    Task 67

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр в числе {number} = {SumRec(number)}");
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + SumRec(n / 10);
+}
+
