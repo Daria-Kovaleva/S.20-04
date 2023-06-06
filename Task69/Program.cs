8d1cc40dcada031471ad918d50479a4e0e299a0b
Darya Kovaleva <dasha_kovaleva@mail.ru>
2023-06-06 16:08:46 +0300

    Task 69

---
+++ Task69/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Write("Введите натуральное число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int square = SquareRec(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} = {square}");
+
+int SquareRec(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * SquareRec(a, b-1);
+}
+
+// int SquareRec(int a, int b)
+// {
+//    return b == 0 ? 1 : a * SquareRec(a, b-1);
+// }
